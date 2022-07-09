102d2e14d7d45440467205557e1ae02f690c1abb
CesarABM <chepiro1996@gmail.com>
2022-07-08 21:20:55 -0500

    Enviar menu desde el repo local

---
+++ Menu.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace TheCapitalBurguer.Models
+{
+    [Table("Menu")]
+    public partial class Menu
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        public string Ingredientes { get; set; }
+
+        public string Tamaño { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal? Precio { get; set; }
+    }
+}
